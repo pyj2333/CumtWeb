f68eda02901022018111d0cc2def2750a2959d60
pyj2333 <3413125076@qq.com>
2020-12-10 16:57:32 +0800

    second

--- src/main/java/app/AppConfig.java
+++ src/main/java/app/AppConfig.java
@@ -6,7 +6,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.*;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.*;
 
@@ -66,49 +65,5 @@ public class AppConfig {
             }
         };
     }
-
-    // 配置缓存
-    @Bean(name = "redisTemplate")
-    public RedisTemplate initRedisTemplate() {
-        JedisPoolConfig poolConfig = new JedisPoolConfig();
-        // 最大空闲数
-        poolConfig.setMaxIdle(50);
-        // 最大连接数
-        poolConfig.setMaxTotal(100);
-        // 最大等待亳秒数
-        poolConfig.setMaxWaitMillis(20000);
-        // 创建Jedis连接工厂
-        JedisConnectionFactory connectionFactory = JedisConnectionFactory(poolConfig);
-        connectionFactory.setHostName("localhost");
-        connectionFactory.setPort(6379);
-        // 调用后初始化方法，没有它将抛出异常
-        connectionFactory.afterPropertiesSet();
-        // 自定Redis序列化器
-        RedisSerializer jdkSerializationRedisSerializer = new JdkSerializationRedisSerializer();
-        RedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        // 定义RedisTemplate，并设置连接工程
-        RedisTemplate redisTemplate = new RedisTemplate();
-        redisTemplate.setConnectionFactory(connectionFactory);
-        // 设置序列化器
-        redisTemplate.setDefaultSerializer(stringRedisSerializer);
-        redisTemplate.setKeySerializer(stringRedisSerializer);
-        redisTemplate.setValueSerializer(jdkSerializationRedisSerializer);
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        redisTemplate.setHashValueSerializer(jdkSerializationRedisSerializer);
-        return redisTemplate;
-    }
-    @Bean(name = "redisCacheManager")
-    public CacheManager initRedisCacheManager(@Autowired RedisTemplate redisTempate) {
-        RedisCacheManager cacheManager = new RedisCacheManager(redisTempate);
-        // 设置超时时间为10分钟，单位为秒
-        cacheManager.setDefaultExpiration(600);
-        // 设置缓存名称
-        List<String> cacheNames = new ArrayList<String>();
-        cacheNames.add("redisCacheManager");
-        cacheManager.setCacheNames(cacheNames);
-        return cacheManager;
-    }
-
-
 }
 
